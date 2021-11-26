677ff12d3b1f37d7fc7d0d91376832c426cc29d9
Anatoli Dimitrov <apdimitrov@yahoo.com>
2021-11-26 15:22:49 +0200

    Started EXAM PREP 08 August 2020.

---
+++ MS SQL/MSSQL - Advanced/Exam Preparations/C# DB Advanced Exam - 08 August 2020/Model Definition_Skeleton + Datasets/VaporStore/Data/Models/GameTag.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using VaporStore.Data.Models;
+
+namespace VaporStore.Data
+{
+    public class GameTag
+    {
+        [ForeignKey(nameof(Game))]
+        public int GameId { get; set; }
+        public Game Game { get; set; }
+
+        [ForeignKey(nameof(Tag))]
+        public int TagId { get; set; }
+        public Tag Tag { get; set; }
+    }
+}
