d760cd98bcce08f577584ba686209193adbeab06
Anatoli Dimitrov <apdimitrov@yahoo.com>
2022-03-03 14:00:12 +0200

    Updates on the UI and pics uploaded - ASP.NET

---
+++ ASP.NET Core/Claudi/Claudi.Web/Controllers/ColorsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Claudi.Web.Controllers
+{
+    public class ColorsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Horizontal()
+        {
+            return View("Multiple");
+        }
+    }
+}
